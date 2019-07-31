e128ec241d68035c7e718087334226f631860951
Alan Hussey <alan.hussey@gmail.com>
2019-07-31 16:03:13 -0700

    show a ranking of teams by mascot weight

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import minBy from "lodash/minBy";
 import sortBy from "lodash/sortBy";
-import React, { Fragment, useState, useEffect } from "react";
+import mapValues from "lodash/mapValues";
+import React, { useState, useEffect } from "react";
 import { luminance } from "./color";
 import "./App.css";
-
-function get(url) {
-  return fetch(url).then(response => response.json());
-}
 
 function Rank({ team, rank, children }) {
   const { school, mascot, logos, color, alt_color } = team;
@@ -42,38 +39,141 @@ function PollRanks({ poll, ranks, teams }) {
   );
 }
 
+class DataSource {
+  constructor(options) {
+    this.name = options.name;
+    this.description = options.description || "";
+    this.id = options.id;
+    this.initialState = options.initialState || null;
+  }
+  fetch() {
+    return Promise.reject(
+      new TypeError(`${this.constructor} does not implement \`fetch\``)
+    );
+  }
+  process(data) {
+    return data;
+  }
+}
+
+class FetchDataSource extends DataSource {
+  constructor(options) {
+    super(options);
+    this.href = `/data/${options.href}`;
+    if (options.process) {
+      this.process = options.process;
+    }
+  }
+  fetch() {
+    return fetch(this.href).then(response => response.json());
+  }
+}
+
 const DATA_SOURCES = {
-  fbsTeams: "/data/fbs-teams.json",
-  currentRankings: "/data/rankings/current.json"
+  fbsTeams: new FetchDataSource({
+    name: "All FBS teams",
+    href: "fbs-teams.json",
+    initialState: {}
+  }),
+  currentRankings: new FetchDataSource({
+    name: "Most recent polls",
+    href: "rankings/current.json",
+    initialState: { polls: [] }
+  }),
+  mascotWeights: new FetchDataSource({
+    name: "Mascot weights",
+    description:
+      "Per Jon Bois and SBNation https://www.youtube.com/watch?v=obtRtrk42a8",
+    href: "mascot-weights.json",
+    initialState: {
+      teams: {},
+      weights: []
+    },
+    process(data) {
+      const weights = sortBy(Object.values(data), n => -n);
+      return {
+        teams: mapValues(data, weight => ({
+          weight: Number(weight), // parse `"Infinity"`
+          rank: weights.indexOf(weight) + 1
+        }))
+      };
+    }
+  })
 };
 
 function App() {
-  const [state, setState] = useState({ data: {} });
+  const [state, setState] = useState({
+    data: mapValues(DATA_SOURCES, source => source.initialState)
+  });
 
-  useEffect(
-    () =>
-      Promise.all(
-        Object.entries(DATA_SOURCES).map(([key, url]) =>
-          get(url).then(json => [key, json])
-        )
-      ).then(data => setState({ data: Object.fromEntries(data) })),
-    []
-  );
+  useEffect(() => {
+    Promise.all(
+      Object.entries(DATA_SOURCES).map(([key, source]) =>
+        source.fetch().then(data => [key, source.process(data)])
+      )
+    ).then(data => setState({ data: Object.fromEntries(data) }));
+  }, []);
 
-  const teams = state.data.fbsTeams || {};
-  const rankings = state.data.currentRankings || {};
-  const polls = rankings.polls || [];
+  const { mascotWeights, fbsTeams, currentRankings } = state.data;
+  const { polls } = currentRankings;
   return (
-    <div>
-      <h1>Polls</h1>
-      <p>
-        {rankings.season} week {rankings.week}
-      </p>
+    <div style={{ columns: "300px 3" }}>
+      <div style={{ "break-inside": "avoid" }}>
+        <h2>{DATA_SOURCES.mascotWeights.name}</h2>
+        <p>{DATA_SOURCES.mascotWeights.description}</p>
+        <ol className="App-ranks">
+          {sortBy(
+            Object.values(fbsTeams),
+            team => -mascotWeights.teams[team.school].weight
+          )
+            .slice(0, 25)
+            .map(team => {
+              const { weight } = mascotWeights.teams[team.school];
+              return (
+                <Rank
+                  key={team.school}
+                  team={team}
+                  rank={{ rank: mascotWeights.teams[team.school].rank }}
+                >
+                  {Number.isFinite(weight) ? (
+                    `${
+                      weight > 100
+                        ? `${weight}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                        : weight
+                    } lbs`
+                  ) : (
+                    <em>Unknowable</em>
+                  )}
+                </Rank>
+              );
+            })}
+          <small>
+            Also receiving votes:
+            <br />
+            {sortBy(
+              Object.values(fbsTeams),
+              team => -mascotWeights.teams[team.school].weight
+            )
+              .slice(25, 30)
+              .map(
+                team =>
+                  `${team.school} ${team.mascot} (${
+                    mascotWeights.teams[team.school].weight
+                  } lbs)`
+              )
+              .join(", ")}
+          </small>
+        </ol>
+      </div>
+
       {polls.map(({ poll, ranks }) => (
-        <Fragment key={poll}>
-          <h2>{poll}</h2>
-          <PollRanks poll={poll} ranks={ranks} teams={teams} />
-        </Fragment>
+        <div style={{ "break-inside": "avoid" }} key={poll}>
+          <h3>{poll}</h3>
+          <p>
+            {currentRankings.season} week {currentRankings.week}
+          </p>
+          <PollRanks poll={poll} ranks={ranks} teams={fbsTeams} />
+        </div>
       ))}
     </div>
   );
