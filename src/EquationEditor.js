ff685254cf5b7fd27a6ebfdbc6156a974fef20b6
Alan Hussey <alan.hussey@gmail.com>
2019-08-11 18:56:59 -0700

    use a pretty code editor

--- src/EquationEditor.js
+++ src/EquationEditor.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState, useMemo } from "react";
-import debounce from "lodash/debounce";
-import fromPairs from "lodash/fromPairs";
 import difference from "lodash/difference";
 import { create, all } from "mathjs/number";
+import Editor from "react-simple-code-editor";
+import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
 
 import { DATA_SOURCES } from "./DataSource";
 import Rank from "./Rank";
@@ -11,40 +13,74 @@ import { DESCENDING } from "./constants";
 
 const math = create(all);
 
-// Pre-defined set of built-ins
-const DEFAULT_CONTEXT = {
-  ...fromPairs(Object.getOwnPropertyNames(Math).map(key => [key, Math[key]]))
-};
-
 function makeScoringFn(expression) {
   const node = math.parse(expression);
   const code = node.compile();
   return team => code.evaluate(team);
 }
 
+const INITIAL_EQUATION =
+  "100 * (totalOffense / (totalOffense + totalDefense)) * winningPercentage / PI";
+
+const CODE_FONT_FAMILY =
+  '"Fira code", "Fira Mono", Inconsolata, Menlo, monospace';
+
+const CodeEditor = ({ code, setCode, error }) => (
+  <>
+    <Editor
+      value={code}
+      onValueChange={setCode}
+      highlight={code => highlight(code, languages.js)}
+      padding={10}
+      style={{
+        fontFamily: CODE_FONT_FAMILY,
+        fontSize: 12,
+        border: "1px solid grey"
+      }}
+    />
+    {error && (
+      <p style={{ color: "red" }}>
+        Uh oh! <code>{error.message}</code>
+      </p>
+    )}
+  </>
+);
+
 const render = !!(window.Intl && Intl.NumberFormat)
   ? new Intl.NumberFormat().format
   : value => value.toFixed(3);
 
+function EquationEditorRankedTeams({ teams }) {
+  return (
+    <table className="App-ranks">
+      <thead>
+        <tr>
+          <th>Rank</th>
+          <th>Team</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>
+        {teams.map(({ team, rank, score, ...teamStats }) => (
+          <Rank key={team.school} team={team} rank={rank}>
+            <td title={String(score[0])}>{render(score)}</td>
+          </Rank>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
 const isSubset = (subset, superset) =>
   difference(subset, superset).length === 0;
 
-export default function EquationEditor({
-  factors,
-  addFactors,
-  teams: teamsWithFactors
-}) {
-  const [equation, _setEquation] = useState(
-    "(totalOffense / (totalOffense + totalDefense)) * winningPercentage / PI * 100"
-  );
+export default function EquationEditor({ factors, addFactors, teams }) {
+  const [equation, setEquation] = useState(INITIAL_EQUATION);
 
-  // A (super)set of the factors needed
+  // All the references in the equation that are valid data sources
   const desiredSources = useMemo(() => {
-    const variables =
-      // All named references in the equation...
-      [...new Set(equation.match(/[A-z]+/g))]
-        // that are not already in context
-        .filter(variable => !DEFAULT_CONTEXT.hasOwnProperty(variable));
+    // All named references in the equation
+    const variables = [...new Set(equation.match(/[A-z]+/g))];
 
     return variables
       .map(variable => DATA_SOURCES.find(source => source.key === variable))
@@ -52,10 +88,11 @@ export default function EquationEditor({
   }, [equation]);
 
   useEffect(() => {
-    const desiredFactors = desiredSources.map(({ key }) => key);
-    const availableFactors = factors.map(({ key }) => key);
+    const desiredSourceKeys = desiredSources.map(({ key }) => key);
+    const availableFactorKeys = factors.map(({ key }) => key);
 
-    if (isSubset(desiredFactors, availableFactors)) return;
+    // Early-out if we already are aware of all dependencies
+    if (isSubset(desiredSourceKeys, availableFactorKeys)) return;
 
     addFactors(
       desiredSources.map(source => ({
@@ -65,78 +102,51 @@ export default function EquationEditor({
     );
   }, [addFactors, desiredSources, factors]);
 
-  // Debounce so each keystroke doesn't cause a re-render,
-  // memoize so debounce internal state isn't lost.
-  const setEquation = useMemo(() => {
-    return debounce(_setEquation, 250);
-  }, [_setEquation]);
-
   // Check scoreFn for potential errors before trying to render a list
   const [scoreFn, error] = useMemo(() => {
-    const fn = makeScoringFn(equation);
     try {
-      fn(teamsWithFactors[0]);
+      const fn = makeScoringFn(equation);
+      fn(teams[0]);
       return [fn, null];
     } catch (error) {
       // If there's a problem, produce a "default" score function
       return [() => null, error];
     }
-  }, [equation, teamsWithFactors]);
+  }, [equation, teams]);
 
   // Teams ranked by the given equation
-  const teams = useMemo(
-    () => rankBy(teamsWithFactors, [{ key: scoreFn, order: DESCENDING }]),
-    [scoreFn, teamsWithFactors]
+  const rankedTeams = useMemo(
+    () => rankBy(teams, [{ key: scoreFn, order: DESCENDING }]),
+    [scoreFn, teams]
   );
 
+  // Avoid janky autoformatting
+  const MATH_JS = (
+    <a href="https://mathjs.org/docs/expressions/syntax.html">
+      <code>math.js</code>
+    </a>
+  );
   return (
     <>
-      <textarea
-        style={{
-          width: "calc(100% - 2em)",
-          margin: "1em",
-          fontFamily: '"Fira Code", Inconsolata, Menlo, monospace'
-        }}
-        onChange={event => setEquation(event.target.value)}
-        defaultValue={equation}
-      />
-      {error && <p style={{ color: "red" }}>Uh oh! {error.message}</p>}
+      <p>Do some math!</p>
+      <p>
+        You can write whatever {MATH_JS} code you want here. Any available data
+        source that you reference will be automatically downloaded.
+      </p>
 
-      <p>Available statistics:</p>
-
+      <p>Currently loaded data sources:</p>
       <ol>
         {factors.map(factor => (
           <li key={factor.key}>
-            {DATA_SOURCES.find(source => source.key === factor.key).name} (
-            <code
-              style={{
-                fontFamily: '"Fira Code", Inconsolata, Menlo, monospace',
-                padding: "5px",
-                backgroundColor: "hsla(0, 0%, 0%, .1)"
-              }}
-            >
-              {factor.key}
-            </code>
-            )
+            {DATA_SOURCES.find(source => source.key === factor.key).name}(
+            <code style={{ fontFamily: CODE_FONT_FAMILY }}>{factor.key}</code>)
           </li>
         ))}
       </ol>
-      <table className="App-ranks">
-        <thead>
-          <tr>
-            <th>Rank</th>
-            <th>Team</th>
-            <th>Score</th>
-          </tr>
-        </thead>
-        <tbody>
-          {teams.map(({ team, rank, score, ...teamStats }) => (
-            <Rank key={team.school} team={team} rank={rank}>
-              <td title={score}>{render(score)}</td>
-            </Rank>
-          ))}
-        </tbody>
-      </table>
+
+      <CodeEditor code={equation} setCode={setEquation} error={error} />
+
+      <EquationEditorRankedTeams teams={rankedTeams} />
     </>
   );
 }
